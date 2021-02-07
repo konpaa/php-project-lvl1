7bf97c89954d2418cbfae7ced610f2b8551db0f8
Konpa <pavelkonoplyanikov@yandex.by>
2021-02-08 00:04:45 +0300

    2 parts

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'c43d1d02b071526df8e8eb859966363b6786f237',
+    'name' => 'hexlet/code',
+  ),
+  'versions' => 
+  array (
+    'hexlet/code' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c43d1d02b071526df8e8eb859966363b6786f237',
+    ),
+  ),
+);
